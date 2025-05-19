156a1692ad6861ad68633add83450bd02eadcbe1
computermouth <computermouth@crunchbangplusplus.org>
2025-05-19 18:50:46 -0500

    update traverse

--- src/traverse.c
+++ src/traverse.c
@@ -10,21 +10,27 @@
 /* The following C subroutine visits all voxels along the line
 segment from (x, y, z) and (x + dx, y + dy, z + dz) */
 
-#include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-int sgn(int x) {
+int sign(int x) {
     return (x > 0) - (x < 0);
 }
 
-void VisitVoxel(int x, int y, int z) {}
+void visit(int x, int y, int z) {
+	fprintf(stderr, "[ %d, %d, %d ]\n", x, y, z);
+}
 
 void Line (int x, int y, int z, int dx, int dy, int dz)
 {
+	
+	fprintf(stderr, "start [ %d, %d, %d ]\n", x, y, z);
+	fprintf(stderr, "delta [ %d, %d, %d ]\n", dx, dy, dz);
+	
     int n, sx, sy, sz, exy, exz, ezy, ax, ay, az, bx, by, bz;
 
     // step_xyz
-    sx = sgn(dx);  sy = sgn(dy);  sz = sgn(dz);
+    sx = sign(dx);  sy = sign(dy);  sz = sign(dz);
 
     // abs_delta_xyz
     ax = abs(dx);  ay = abs(dy);  az = abs(dz);
@@ -37,8 +43,8 @@ void Line (int x, int y, int z, int dx, int dy, int dz)
 
     // total_steps
     n = ax+ay+az;
-    while ( n-- ) {
-        VisitVoxel ( x, y, z );
+    for (int i = 0; i <= n; i++ ) {
+        visit ( x, y, z );
         if ( exy < 0 ) {
             if ( exz < 0 ) {
             x += sx;
@@ -60,4 +66,15 @@ void Line (int x, int y, int z, int dx, int dy, int dz)
             }
         }
     }
+}
+
+int main(int argc, char ** argv){
+	
+	if (argc != 7) {
+		fprintf(stderr, "\n  USAGE: %s 1 1 1 2 2 2", argv[0]);
+		return 1;
+	}
+	
+	Line(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]), atoi(argv[6]));
+	return 0;
 }
